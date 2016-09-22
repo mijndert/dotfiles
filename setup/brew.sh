e47eba3a03f38edee94eb7a69efa505896e6d345
Mijndert Stuij <mijndert@mijndertstuij.nl>
2016-09-22 10:45:52 +0200

    Brew casks added

--- setup/brew.sh
+++ setup/brew.sh
@@ -1,57 +1,16 @@
 #!/bin/bash
 
-# Installs Homebrew, Git, git-extras, git-friendly, hub, Node.js, configures Apache, PHP, MySQL, etc.
-
 # Ask for the administrator password upfront
 sudo -v
 
-# Install Homebrew
-command -v brew >/dev/null 2>&1 || ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # Make sure we’re using the latest Homebrew
 brew update
 
-# Upgrade any already-installed formulae
-brew upgrade
-
-# GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
-
-# GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
-brew install tree
-brew install wget
-brew install curl
-
-# Packer
-brew install packer
-
-# More recent versions of some OS X tools
-brew tap homebrew/dupes
-brew install homebrew/dupes/grep
-
-# Git
-brew install git
-brew install git-extras
-
-# Python
-brew install python
-
-# Ansible
-brew install ansible
-
-# Sshuttle
-brew install sshuttle
-
-# Install virtualenv
-pip install virtualenv
-pip install virtualenvwrapper
-
-# Vim
-brew install vim
-
-# Watch
-brew install watch
-
-# Remove outdated versions from the cellar
-brew cleanup
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew bundle
--- setup/virtualenv.sh
+++ setup/virtualenv.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Ask for the administrator password upfront
+sudo -v
+
 # Installs virtualenv and virtualenvwrapper and installs the pip requirements
+
+pip install virtualenv
+pip install virtualenvwrapper
 
 # source virtualenvwrapper again because of weirdness
 source /usr/local/bin/virtualenvwrapper.sh
