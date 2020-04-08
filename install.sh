0fc8b909d16b6743ac52fff3ead07d5b283085ea
Mijndert Stuij <mijndert@mijndertstuij.nl>
2020-04-08 14:40:58 +0200

    add python shit to install script

--- install.sh
+++ install.sh
@@ -16,6 +16,11 @@ if [[ `uname` == 'Darwin' ]]; then
   brew tap homebrew/bundle
   brew bundle
 
+  # Do python stuff
+  ln -s -f /usr/local/bin/pip3 /usr/local/bin/pip
+  ln -s -f /usr/local/bin/python3 /usr/local/bin/python
+  pip install virtualenv
+
   # Install shell
   curl -Lo /tmp/install-ohmyzsh.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
   sh /tmp/install-ohmyzsh.sh --unattended
