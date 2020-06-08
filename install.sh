f3c1eb3ca2ad45fabba7d75623c536a936f6ffdf
Mijndert Stuij <mijndert@mijndertstuij.nl>
2020-06-08 17:44:41 +0200

    move some stuff around

--- install.sh
+++ install.sh
@@ -14,18 +14,18 @@ if [[ $(uname) == 'Darwin' ]]; then
   brew update
   brew tap homebrew/bundle
   brew bundle
-
-  # Do python stuff
-  ln -s -f /usr/local/bin/pip3 /usr/local/bin/pip
-  ln -s -f /usr/local/bin/python3 /usr/local/bin/python
-  pip install virtualenv
-
-  # Install shell
-  curl -Lo /tmp/install-ohmyzsh.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-  sh /tmp/install-ohmyzsh.sh --unattended
-  grep -Fxq '/usr/local/bin/zsh' /etc/shells || sudo bash -c "echo /usr/local/bin/zsh >> /etc/shells"
-  chsh -s /usr/local/bin/zsh "$USER"
 fi
+
+# Do python stuff
+ln -s -f /usr/local/bin/pip3 /usr/local/bin/pip
+ln -s -f /usr/local/bin/python3 /usr/local/bin/python
+pip install virtualenv
+
+# Install shell
+curl -Lo /tmp/install-ohmyzsh.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sh /tmp/install-ohmyzsh.sh --unattended
+grep -Fxq '/usr/local/bin/zsh' /etc/shells || sudo bash -c "echo /usr/local/bin/zsh >> /etc/shells"
+chsh -s /usr/local/bin/zsh "$USER"
 
 # Get the dotfiles installation directory
 DOTFILES_DIR=$(pwd)
