6994b75baa303cf625e6f496a9c3a84ce2f03815
Mijndert Stuij <mijndert@mijndertstuij.nl>
2020-06-27 11:02:16 +0200

    add venvwrapper

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ fi
 # Do python stuff
 ln -s -f /usr/local/bin/pip3 /usr/local/bin/pip
 ln -s -f /usr/local/bin/python3 /usr/local/bin/python
-pip install virtualenv
+pip install virtualenv virtualenvwrapper
 
 # Install shell
 curl -Lo /tmp/install-ohmyzsh.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
