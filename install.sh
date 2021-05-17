6bfcb74e561ddfde7e21515277b776f83661f78b
Mijndert Stuij <mijndert@mijndertstuij.nl>
2021-05-17 08:45:15 +0200

    use brew prefix

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [ ! -f ~/.zshenv ]; then
-  echo "~/.zshenv not found, create it first to set HOMEBREW_DIR to either /opt/homebrew (ARM) or /usr/local (x86)"
-  exit 0
-fi
-
 DOTFILES_DIR=$(pwd)
 
 # Mac-specific stuff goes here
@@ -26,8 +21,8 @@ if [[ $(uname) == 'Darwin' ]]; then
   # Set up shell
   curl -Lo /tmp/install-ohmyzsh.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
   sh /tmp/install-ohmyzsh.sh --unattended
-  grep -Fxq '$HOMEBREW_DIR/bin/zsh' /etc/shells || sudo bash -c "echo $HOMEBREW_DIR/bin/zsh >> /etc/shells"
-  chsh -s $HOMEBREW_DIR/bin/zsh "$USER"
+  grep -Fxq '$(brew --prefix)/bin/zsh' /etc/shells || sudo bash -c "echo $(brew --prefix)/bin/zsh >> /etc/shells"
+  chsh -s $(brew --prefix)/bin/zsh "$USER"
   npm install --global pure-prompt
 fi
 
