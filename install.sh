f5c72350ef74d6888bb883ca30a34bc2ccc9d069
Mijndert Stuij <mijndert@mijndertstuij.nl>
2022-07-01 20:59:36 +0200

    make linux zshrc

--- install.sh
+++ install.sh
@@ -30,10 +30,18 @@ if [[ $(uname) == 'Darwin' ]]; then
   chsh -s $(brew --prefix)/bin/zsh "$USER"
 
   sh ./macos.sh
+
+  ln -sf "$DOTFILES_DIR/bin/tm" $(brew --prefix)/bin/
+  ln -sf "$DOTFILES_DIR/bin/git-up" $(brew --prefix)/bin/
+
+  ln -sf "$DOTFILES_DIR/.zshrc" ~
+fi
+
+if [[ $(uname) == 'Linux' ]]; then
+  ln -sf "$DOTFILES_DIR/.zshrc.linux" ~/.zshrc
 fi
 
 ln -sf "$DOTFILES_DIR/.gitconfig" ~
-ln -sf "$DOTFILES_DIR/.zshrc" ~
 ln -sf "$DOTFILES_DIR/.tmux.conf" ~
 ln -sf "$DOTFILES_DIR/.vimrc" ~
 ln -sf "$DOTFILES_DIR/.ansible.cfg" ~
@@ -42,11 +50,8 @@ ln -sf "$DOTFILES_DIR/.alias" ~
 ln -sf "$DOTFILES_DIR/.functions" ~
 ln -sf "$DOTFILES_DIR/config" ~/.ssh/
 ln -sf "$DOTFILES_DIR/.newsboat" ~
-ln -sf "$DOTFILES_DIR/bin/tm" $(brew --prefix)/bin/
-ln -sf "$DOTFILES_DIR/bin/git-up" $(brew --prefix)/bin/
 
-# Some Vim stuff...
-mkdir ~/.vim/colors
+mkdir -p ~/.vim/colors
 ln -sf $DOTFILES_DIR/nord.vim ~/.vim/colors
 git clone https://github.com/hashivim/vim-terraform.git ~/.vim/pack/plugins/start/vim-terraform
 git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/pack/plugins/start/editorconfig-vim
